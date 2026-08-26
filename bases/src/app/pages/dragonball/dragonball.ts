6386c4e2cfbae0621ad725b8de9157d3132280c8
Marcosdaag <marcosoffs99@gmail.com>
2026-08-26 12:51:03 -0300

    fix

--- bases/src/app/pages/dragonball/dragonball.ts
+++ bases/src/app/pages/dragonball/dragonball.ts
@@ -12,17 +12,35 @@ interface Character {
   templateUrl: './dragonball.html',
 })
 export class Dragonball {
+  name  = signal('');
+  power = signal(0);
+
   characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8001 },
+    { id: 1, name: 'Goku',    power: 9001 },
+    { id: 2, name: 'Vegeta',  power: 8001 },
     { id: 3, name: 'Picollo', power: 3001 },
-    { id: 4, name: 'Yamcha', power: 500 },
+    { id: 4, name: 'Yamcha',  power: 500  },
   ]);
 
-  powerClasses = computed(() => {
-    return {
-      'text-danger': true,
+  powerClasses = computed(() => ({
+    'text-danger':  this.power() > 9000,
+    'text-primary': this.power() <= 9000,
+  }));
+
+  addCharacter() {
+    if (!this.name() || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id:    this.characters().length + 1,
+      name:  this.name(),
+      power: this.power(),
     };
-  });
+
+    this.characters.update(list => [...list, newCharacter]);
+
+    // Resetear el formulario
+    this.name.set('');
+    this.power.set(0);
+  }
 
 }
