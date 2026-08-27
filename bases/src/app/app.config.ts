b00a9dff3b06658f60bcea274cce6b7e3e57f0be
Marcosdaag <marcosoffs99@gmail.com>
2026-08-27 17:48:01 -0300

    feat: hash strategy

--- bases/src/app/app.config.ts
+++ bases/src/app/app.config.ts
@@ -2,11 +2,18 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    // HashStrategy, de esta manera evitamos que el navegador intente redireccionar a otra carpeta
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    },
   ]
 };
--- bases/src/app/services/dragonball.service.ts
+++ bases/src/app/services/dragonball.service.ts
@@ -1,12 +1,24 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/characterinterface';
+
+
+const loadFromLocalstorage = (): Character[] => {
+
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+
+}
+
 
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-  ]);
+  characters = signal<Character[]>(loadFromLocalstorage());
+
+  saveToLocalStorage = effect(() => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  })
+
 
   addCharacter(character: Character) {
     this.characters.update(list => [...list, character]);
